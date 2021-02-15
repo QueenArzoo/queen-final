f1b8f6448c966a26f19b8bc82a1501eefb4f5e3f
Terabyte <71519396+okgtfo@users.noreply.github.com>
2021-02-15 10:27:44 +0530

    Update songs.py

--- SaitamaRobot/modules/songs.py
+++ SaitamaRobot/modules/songs.py
@@ -40,7 +40,7 @@ except:
 	from youtubesearchpython import SearchVideos 
 	pass
 
-@register(pattern="^!ssgg (.*)")
+@register(pattern="^!song (.*)")
 async def download_video(v_url):
 
     lazy = v_url ; sender = await lazy.get_sender() ; me = await lazy.client.get_me()
@@ -165,7 +165,7 @@ async def download_video(v_url):
         os.remove(f"{rip_data['id']}.mp4")
         await rkp.delete()
 
-@register(pattern="^!videeoso (.*)")
+@register(pattern="^!video (.*)")
 async def download_video(v_url):  
     lazy = v_url ; sender = await lazy.get_sender() ; me = await lazy.client.get_me()
     if not sender.id == me.id:
